ba657bc4bb3c67699015845c469e116aa2170af0
Daniel Semblano <dsemblano@gmail.com>
2022-08-23 10:54:11 -0300

    Sage 10 theme, classes, etc

--- site/web/app/themes/179eventos/app/filters.php
+++ site/web/app/themes/179eventos/app/filters.php
@@ -12,5 +12,55 @@ namespace App;
  * @return string
  */
 add_filter('excerpt_more', function () {
-    return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
+    return sprintf(' &hellip; <p class="mt-2"><a href="%s">%s</a></p>', get_permalink(), __('Leia Mais', 'sage'));
+});
+
+
+// remove url from commment
+add_filter( 'comment_form_default_fields', function($fields) {
+    if(isset($fields['url']))
+    unset($fields['url']);
+    return $fields;
+});
+
+/**
+* Hook to the the content filter to modify/improve the output content * and hotfix some bugs which are present in Gutenberg
+*/
+add_filter('the_content', static function( string $content ) : string {
+
+$replace = [
+'noreferrer' => '',
+'noreferrer' => ''
+];
+
+$content = strtr($content, $replace);
+return $content;
+});
+
+// default target _blank
+function default_target_blank() {
+ 
+    ?>
+    <script>
+        jQuery(document).on( 'wplink-open', function( wrap ) {
+            if ( jQuery( 'input#wp-link-url' ).val() <= 0 )
+                jQuery( 'input#wp-link-target' ).prop('checked', true );
+        });
+    </script>
+    <?php
+}
+add_action( 'admin_footer-post-new.php', 'default_target_blank', 10, 0 );
+add_action( 'admin_footer-post.php', 'default_target_blank', 10, 0 );
+
+/**
+ * Filter the excerpt length to 50 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+add_filter( 'excerpt_length', function($length) {
+    if ( is_admin() ) {
+        return $length;
+}
+return 20;
 });
---
+++ site/web/app/themes/179eventos/resources/views/partials/home/section1.blade.php
@@ -0,0 +1,7 @@
+<section id="section1" class="container px-8 lg:px-16 flex flex-col lg:flex-row lg:mt-40 justify-between items-start bg-white relative">
+  <!--Left Col-->
+  <div class="flex flex-col w-full animate__animated animate__fadeInLeftBig">
+    <h1 class="logonumero mb-4 uppercase text-7xl lg:text-9xl font-bold text-pros90blue">Eventos 179</h1>
+  </div>
+  <img class="block" width="600" height="718" alt="Logo Eventos 179" src="@asset('images/logo.png')" />
+</section>
