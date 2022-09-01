41b79f25fa2ba60345f36ce68eb63588cfab1df1
Daniel Semblano <dsemblano@gmail.com>
2022-09-01 14:23:56 -0300

    home background and logo

--- site/web/app/themes/179eventos/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/179eventos/resources/views/partials/home/section1.blade.php
@@ -2,6 +2,6 @@
   <!--Left Col-->
   <div class="flex flex-col items-center w-full animate__animated animate__fadeInLeftBig">
     <h1 class="logonumero mb-4 uppercase text-7xl lg:text-9xl font-bold text-pros90blue">179 Eventos</h1>
-    <img class="block" width="119" height="210" alt="Logo Eventos 179" src="@asset('images/logo_sand.png')" />
+    <img class="block" alt="Logo Eventos 179" src="@asset('images/logo_sand.png')" />
   </div>
 </section>
