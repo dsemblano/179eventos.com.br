405eedcc2c3a1c40211422adee97490a6995931b
Daniel Semblano <dsemblano@gmail.com>
2022-09-06 18:05:59 -0300

    unfixed menu

--- site/web/app/themes/179eventos/resources/views/sections/header.blade.php
+++ site/web/app/themes/179eventos/resources/views/sections/header.blade.php
@@ -11,7 +11,7 @@
 </header> --}}
 
 
-<header class="banner fixed top-0 w-full z-50 pt-20">
+<header class="banner top-0 w-full z-50 pt-20">
   <nav class="nav-primary container py-2 px-4 lg:px-16">
     <div class="flex flex-wrap lg:flex-nowrap justify-center items-center mx-auto">
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'flex flex-col mt-4 hidden lg:flex items-center font-bold lg:flex-row lg:space-x-8 lg:mt-0 lg:text-2xl text-white uppercase', 'echo' => false]) !!}
