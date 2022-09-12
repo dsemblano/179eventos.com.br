41c58575f4a5f4e2b9d47bcaf9804e3f418a3e29
Daniel Semblano <dsemblano@gmail.com>
2022-09-12 12:16:58 -0300

    Wordpress update

--- site/web/app/themes/179eventos/resources/views/template-home.blade.php
+++ site/web/app/themes/179eventos/resources/views/template-home.blade.php
@@ -5,6 +5,15 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+  $rnd = rand (1,3);
+  // $pos = ($rnd == 1) ? "50px 0;" : ($rnd == 3) ? "20px 0;" : "top left";    
+@endphp
+<style type="text/css">
+body {
+  background: url('../images/background-<?= $rnd; ?>.jpg') no-repeat center center fixed;
+}
+</style>
     @include('partials/home.section1')
     {{-- {!! do_shortcode('[wdi_feed id="1"]') !!} --}}
     {{-- {!! do_shortcode('[instagram-feed feed=1 class="container px-8 lg:px-16"]') !!} --}}
