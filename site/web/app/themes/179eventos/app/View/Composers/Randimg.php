d984616151f17a1ede3129bf1354b9bc04844fa6
Daniel Semblano <dsemblano@gmail.com>
2022-09-12 15:04:40 -0300

    dynamic logo and background

---
+++ site/web/app/themes/179eventos/app/View/Composers/Randimg.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\View\Composers;
+
+use Roots\Acorn\View\Composer;
+
+class Randimg extends Composer
+{
+    /**
+     * This tells the Composer that it should bind data to the 'example'
+     * partial.
+     */
+    protected static $views = [
+        'partials.section1',
+    ];
+
+        /**
+     * Data to be passed to view before rendering, but after merging.
+     *
+     * @return array
+     */
+    public function override()
+    {
+        return [
+            'rnd' => $this->rnd(),
+            'randLogo' => $this->randLogo(),
+            'randBg' => $this->randBg(),
+        ];
+    }
+    
+    /**
+     * This will make the variable `$roots` available in the 'example' partial
+     * with the value described here.
+     */
+
+    public function rnd()
+    {
+        return rand (1,3);
+    }
+
+    public function randLogo()
+    {
+        $randimg = $this->rnd();
+        return \Roots\asset('images/logo_sand-'.$randimg.'.png');
+    }
+
+    public function randBg()
+    {
+        $randimg = $this->rnd();
+        return \Roots\asset('images/background-'.$randimg.'.jpg');
+    }
+
+    // public function bgimg()
+    // {
+    //     $randimg = Randimg::rnd()->$rnd;
+    //     $bgimg = \Roots\asset('images/background-'.$randimg.'jpg');
+    //     return $bgimg;
+    // }
+}
--- site/web/app/themes/179eventos/resources/views/template-home.blade.php
+++ site/web/app/themes/179eventos/resources/views/template-home.blade.php
@@ -5,16 +5,7 @@
 @extends('layouts.app')
 
 @section('content')
-@php
-  $rnd = rand (1,3);
-  // $pos = ($rnd == 1) ? "50px 0;" : ($rnd == 3) ? "20px 0;" : "top left";    
-@endphp
-<style type="text/css">
-body {
-  background: url('../images/background-<?= $rnd; ?>.jpg') no-repeat center center fixed;
-}
-</style>
-    @include('partials/home.section1')
+    @include('partials/section1')
     {{-- {!! do_shortcode('[wdi_feed id="1"]') !!} --}}
     {{-- {!! do_shortcode('[instagram-feed feed=1 class="container px-8 lg:px-16"]') !!} --}}
     {{-- {!! do_shortcode('[contact-form-7 id="20" title="Formulario"]') !!} --}}
